1e6c5422f571d9a68dabfb453ed458699628123a
nuray-alam <siddik4x@gmail.com>
2021-11-09 23:59:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors())
+app.use(express.json());
